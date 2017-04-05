c7ca983ca282668d1796c03acd0f54c47b78671b
‪‪ ‪‪‪ < >
2017-04-05 19:04:09 -0400

    scheduling system includes adding stuff to calendar and time of scheduling. forgot to ask for user id though. that can be shared with quick message

---
+++ Scheduling/demo/demo.js
@@ -0,0 +1,82 @@
+//SPAGHETTi!
+var start;
+var end;
+var MtrDatepicker1 = (function() {
+
+	var datepickers = [];
+	var init = function(config, settings) {
+		var datepicker = new MtrDatepicker(config);
+		datepickers.push(datepicker);
+
+		var exportFormatsContainer = document.getElementById(settings.exportFormats);
+	 	datepickerChange(exportFormatsContainer, datepicker);
+
+	 	datepicker.onChange('all', function() {
+			datepickerChange(exportFormatsContainer, datepicker);
+		});
+
+
+		return datepicker;
+	};
+
+	/**
+	 * Dump the datepicker date in different formats
+	 */
+	function datepickerChange(resultElement, datepicker) {
+		var result =
+			datepicker.toDateString() + '<br />' +
+			datepicker.toLocaleDateString() + '<br /><br />' +
+
+			datepicker.toTimeString() + '<br />' +
+      datepicker.getFullTime() + '<br />' +
+      datepicker.format('M/D/YYYY hh:mm A') + '<br />' +
+			datepicker.format('YYYY-MM-DD HH:mm') + '<br />';
+
+		//resultElement.innerHTML = datepicker.format('YYYY-MM-DD HH:mm');
+		start = datepicker.toISOString();
+	}
+
+	return {init: init};
+
+})();
+
+
+var MtrDatepicker2 = (function() {
+
+	var datepickers = [];
+	var init = function(config, settings) {
+		var datepicker = new MtrDatepicker(config);
+		datepickers.push(datepicker);
+
+		var exportFormatsContainer = document.getElementById(settings.exportFormats);
+	 	datepickerChange(exportFormatsContainer, datepicker);
+
+	 	datepicker.onChange('all', function() {
+			datepickerChange(exportFormatsContainer, datepicker);
+		});
+
+
+		return datepicker;
+	};
+
+	/**
+	 * Dump the datepicker date in different formats
+	 */
+	function datepickerChange(resultElement, datepicker) {
+		var result =
+			datepicker.toDateString() + '<br />' +
+			datepicker.toLocaleDateString() + '<br /><br />' +
+
+			datepicker.toTimeString() + '<br />' +
+      datepicker.getFullTime() + '<br />' +
+      datepicker.format('M/D/YYYY hh:mm A') + '<br />' +
+			datepicker.format('YYYY-MM-DD HH:mm') + '<br />';
+
+		//resultElement.innerHTML = datepicker.format('YYYY-MM-DD HH:mm');
+		end = datepicker.toISOString();
+	}
+
+	return {init: init};
+
+})();
+
